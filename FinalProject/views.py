e066cfd9ead9aec4af5c1fb51ddc4004b6182c29
KowsarGhassemian <kowsar.ghassemian@gmail.com>
2024-03-05 12:30:45 +0330

    primary changes

---
+++ FinalProject/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from .models import User, critic, movie
+from .forms import criticForm #اینها با توجه به اسامی ای که در بخش های مدل و غیره می زنیم زیاد و کم میشن
+
+# Create your views here.
+def createc(request): #createc means create critic/ so createm means create movie
+    pass
+
+
+
+def retrievec(request, id):
+    pass
+
+
+def updatec(request):
+    pass
+
+
+def deletec(request, id):
+    pass
+
+
+def createm(request):
+    pass
+
+
+def retrievem(request, id):
+    pass
+
+
+def updatem(request, id):
+    pass
+
+
+def deletem(request, id):
+    pass
+
+def usersignup(request):
+    pass
+
+
+def userlogin(request):
+    pass
+
+
+def userlogout(request):
+    pass
+
+
