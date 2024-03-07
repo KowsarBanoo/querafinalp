fcb88d7ffb3e33663262397f136ffc5b9f69c9ad
Niloufar <niloufarsharifkazemi@gmail.com>
2024-03-08 00:07:13 +0330

    update view and urls

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import usersignup, userlogin, userlogout
 
 urlpatterns= [
-    # path('new-critic/', )
+    path('new-critic/', )
+    path('signup/', usersignup, name='register'),
+    path('login/', userlogin, name='login'),
+    path('logout/', userlogout, name='logout')
 ]
